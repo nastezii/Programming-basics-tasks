048708a54046f19347378fa9ada190787082d420
Shalak Anastasia <nastiashalak@gmail.com>
2024-11-26 16:54:53 +0200

    started working and creating data.csv

---
+++ character strings and files/Program.cs
@@ -0,0 +1,5 @@
+﻿
+
+string[] charCommands = { "all", "lower", "alpha", "alnum" };
+string[] stringCommands = {"print", "set", "substr", "upper", "contains"};
+string[] csvCommands = {"load", "text", "table", "entities", "get", "set", "save"};
