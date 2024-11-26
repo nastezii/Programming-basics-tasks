f5f2259e05b97302f366cda49cce0c952ee37c0c
Shalak Anastasia <nastiashalak@gmail.com>
2024-11-27 00:45:19 +0200

    add initial values and entry point

--- character strings and files/Program.cs
+++ character strings and files/Program.cs
@@ -4,48 +4,42 @@ string[] charCommands = { "all", "lower", "alpha", "alnum" };
 string[] stringCommands = { "print", "set", "substr", "upper", "contains" };
 string[] csvCommands = { "load", "text", "table", "entities", "get", "set", "save" };
 
+//Initial values:
+string currentRow = "";
+string textFromFile = "";
+string[,] convertedArray = ConvertToTable();
+Inventor[] inventors = CreateStructOfInventors();
+
+//Entry point into the program:
+
+while (true)
+{
+    Console.WriteLine("Enter command:");
+    string input = Console.ReadLine();
+    string[] subCommands = input.Split(':');
+    if (subCommands[0] == "exit")
+    {
+        break;
+    }
+    if (subCommands[0] == "char")
+    {
+        CharProcess(subCommands);
+    }
+    else if (subCommands[0] == "string")
+    {
+        StringProcess(subCommands);
+    }
+    else if (subCommands[0] == "csv")
+    {
+        CsvProcess(subCommands);
+    }
+    else
+    {
+        Console.WriteLine($"Class command {subCommands[0]} do not exist.");
+    }
+}
 
 // 1 part :
-
-void CharPrintAll()
-{
-    Console.WriteLine("All ASCII table elements:");
-    for (int i = 0; i <= 127; i++)
-    {
-        Console.WriteLine($"{i}: {Convert.ToChar(i)}.");
-    }
-}
-
-void CharPrintLower()
-{
-    Console.WriteLine("All lower ASCII table elements:");
-    for (int i = 97; i <= 122; i++)
-    {
-        Console.WriteLine($"{i}: {Convert.ToChar(i)}.");
-    }
-}
-
-void CharPrintAlpha()
-{
-    Console.WriteLine("All alpha ASCII table elements:");
-    for (int i = 65; i <= 90; i++)
-    {
-        Console.WriteLine($"{i}: {Convert.ToChar(i)}.");
-    }
-    for (int i = 97; i <= 122; i++)
-    {
-        Console.WriteLine($"{i}: {Convert.ToChar(i)}.");
-    }
-}
-
-void CharPrintAlnum()
-{
-    Console.WriteLine("All numbers in ASCII table:");
-    for (int i = 48; i <= 57; i++)
-    {
-        Console.WriteLine($"{i}: {Convert.ToChar(i)}.");
-    }
-}
 
 void CharProcess(string[] subCommands)
 {
@@ -68,3 +62,44 @@ void CharProcess(string[] subCommands)
         }
     }
 }
+
+void CharPrintAll()
+{
+    Console.WriteLine("All ASCII table elements:");
+    for (int i = 0; i <= 127; i++)
+    {
+        Console.WriteLine($"{i}: {Convert.ToChar(i)}");
+    }
+}
+
+void CharPrintLower()
+{
+    Console.WriteLine("All lower ASCII table elements:");
+    for (int i = 97; i <= 122; i++)
+    {
+        Console.WriteLine($"{i}: {Convert.ToChar(i)}");
+    }
+}
+
+void CharPrintAlpha()
+{
+    Console.WriteLine("All alpha ASCII table elements:");
+    for (int i = 65; i <= 90; i++)
+    {
+        Console.WriteLine($"{i}: {Convert.ToChar(i)}");
+    }
+    for (int i = 97; i <= 122; i++)
+    {
+        Console.WriteLine($"{i}: {Convert.ToChar(i)}");
+    }
+}
+
+void CharPrintAlnum()
+{
+    Console.WriteLine("All numbers in ASCII table:");
+    for (int i = 48; i <= 57; i++)
+    {
+        Console.WriteLine($"{i}: {Convert.ToChar(i)}");
+    }
+}
+
